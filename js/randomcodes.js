cd4ec056d5d0e55165276649a6c31a2394ece07a
Jikang Qu <jikangqu@MacBook-Pro-2.local>
2025-09-23 10:57:48 -0700

    Disable button

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,6 +1,10 @@
+var code = ' ';
+var getCode = ' ';
+var btnvalue;
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
 function generateCode() {
-  var code = "";
-  var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+  code=" ";
   for (var i = 1; i <= 8; i++) {
     var char = Math.floor(Math.random() * str.length);
     code += str.charAt(char);
@@ -14,4 +18,15 @@ function disableButton() {
   document.getElementById("submit").disabled = true;
 }
 
-disableButton();
+function disableButton(btnvalue){
+  document.getElementById("submit").disabled = btnvalue; // able/disable button
+  if (btnvalue == true) { // test if button is disabled or enabled
+    // set button and label color translucent
+    document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+    document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+  } else {
+    // set button and label color with no transparency
+    document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+    document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+  }
+};
